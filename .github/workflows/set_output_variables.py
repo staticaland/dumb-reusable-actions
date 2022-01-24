c7b3a92ae0eee099bf41650953f3d94df68fece0
Anders Knaplund Pettersen <anders@firi.com>
2022-01-24 21:21:15 +0100

    OK

--- .github/workflows/set_output_variables.py
+++ .github/workflows/set_output_variables.py
@@ -1,14 +1,31 @@
+import json
+import collections
+import datetime
+
+
 def set_output(key, value):
-    return f"::set-output name={key}::{value}"
+    return f'::set-output name={key}::"{value}"'
 
 
 def main():
 
-    outputs = { "version": "1.0", "author": "Mickey Mouse"}
-    
-    for key, value in outputs.items():
+    default_config = {
+        "version": "1.0",
+        "author": "Mickey Mouse",
+        "deploy_date": datetime.datetime.now().replace(microsecond=0).isoformat(),
+    }
+
+    with open("project_config.json") as f:
+        project_config = json.load(f)
+
+    # Project config takes precedence
+    combined_config = collections.ChainMap(project_config, default_config)
+
+    # GitHub Actions watches stdout for ::set-output
+    for key, value in combined_config.items():
         s = set_output(key, value)
         print(s)
+
 
 if __name__ == "__main__":
     main()
